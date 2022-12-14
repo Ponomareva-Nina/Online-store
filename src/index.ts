049f35f12520600d5a51229e4055da7fc2cd6baa
Nina Ponomareva <nina.9412@mail.ru>
2022-12-14 21:33:41 +0300

    feat: create project catalogue

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,1 @@
-console.log('Hello, world!');
-import mute from './assets/images/mute.png';
-
 import './global.scss';
-const img = document.createElement('img');
-img.setAttribute('src', mute);
-document.body.append(img);
