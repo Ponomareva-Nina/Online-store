8eb943e6ee8d46f7aa3ffe9866513519086c499a
Nina Ponomareva <nina.9412@mail.ru>
2022-12-23 04:08:56 +0300

    feat: update appController

--- src/components/app/app.ts
+++ src/components/app/app.ts
@@ -1,1 +1,36 @@
-console.log('hello app');
+import Router from '../router/router';
+import CartView from '../views/CartView';
+import StartPageView from '../views/StartPageView';
+import StoreView from '../views/StoreView';
+import Header from '../views/HeaderView';
+import createElem from '../../utils/utils';
+import { AppControllerInterface } from '../../types/interfaces';
+import { routeComponents } from '../../types/types';
+
+export default class AppController implements AppControllerInterface {
+    cartView: CartView;
+    storeView: StoreView;
+    startPage: StartPageView;
+    header: Header;
+    mainContainer: HTMLElement;
+    router: Router;
+
+    constructor() {
+        this.header = new Header(this);
+        this.mainContainer = createElem('main', 'main');
+        this.cartView = new CartView(this);
+        this.startPage = new StartPageView(this);
+        this.storeView = new StoreView(this);
+        this.router = new Router(this);
+    }
+
+    start() {
+        document.body.append(this.header.createHeader(), this.mainContainer);
+        this.router.init();
+    }
+
+    updateCurrentPage(pageId: routeComponents) {
+        this.mainContainer.innerHTML = '';
+        this.mainContainer.append(this[pageId].render());
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,5 @@
+import AppController from './components/app/app';
 import './global.scss';
-import Router from './components/router/router';
-import routes from './components/router/routes';
 
-const router = new Router(routes);
-const test1 = <HTMLElement>document.querySelector('.btn1');
-if (test1) {
-    test1.addEventListener('click', () => {
-        router.loadRoute('');
-    });
-}
-const test2 = <HTMLElement>document.querySelector('.btn2');
-if (test2) {
-    test2.addEventListener('click', () => {
-        router.loadRoute('about');
-    });
-}
-const test3 = <HTMLElement>document.querySelector('.btn3');
-if (test3) {
-    test3.addEventListener('click', () => {
-        router.loadRoute('contact');
-    });
-}
-const test4 = <HTMLElement>document.querySelector('.btn4');
-if (test4) {
-    test4.addEventListener('click', () => {
-        router.loadRoute('products', '1');
-    });
-}
-const test5 = <HTMLElement>document.querySelector('.btn5');
-if (test5) {
-    test5.addEventListener('click', () => {
-        router.loadRoute('products', '2');
-    });
-}
+const app = new AppController();
+app.start();
