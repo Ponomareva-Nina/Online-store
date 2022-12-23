9266ba6ef7d12ae2b2891d1e02dbec42ea3ae739
Nina Ponomareva <nina.9412@mail.ru>
2022-12-24 01:34:33 +0300

    feat: update function match in router

--- src/components/router/router.ts
+++ src/components/router/router.ts
@@ -16,16 +16,32 @@ class Router implements RouterInterface {
         this.navigate();
     }
 
+    public addProp() {
+        //добавляет пропс при клике в адресную строку (проверки:
+        // 1 - если в пропсах нет ничего кроме id - то через ?, если есть - &)
+        // 2 - если добавляем id - добавляем через /
+        // 3 - если добавляем в свойство в котором уже не пустой массив/undefined - то через | стрелку
+    }
+
+    private handleRouteWithProps(pathname: string) {
+        // читает пропсы из адресной строки и добавляет в объект props Route-а
+        const pathParams = pathname.split('/');
+        console.log(pathParams);
+    }
+
     private match(pathname: string) {
         const matchedRoute = this.routes.find((route: Route) => {
             if (route.pageName === pathname) {
                 return route;
-            } else {
-                const pathParams = pathname.split('/');
-                console.log(pathParams);
             }
         });
-        return matchedRoute;
+
+        if (matchedRoute) {
+            return matchedRoute;
+        } else {
+            const matchedRouteWithParams = this.handleRouteWithProps(pathname);
+            return matchedRouteWithParams;
+        }
     }
 
     public navigate() {
